7f81dfa8a283874e03301310b3adf1a2401a30bb
Lenson-Lee <dmstjs7437@naver.com>
2021-07-26 11:55:15 +0900

    [21.07.26]카드뒤집기

--- 3. 종합/card_game/app.js
+++ 3. 종합/card_game/app.js
@@ -5,7 +5,8 @@ const horizon = 4;
 const vertical = 3;
 
 //카드감싸는 cards 요소 검색
-const $cards = document.getElementById('cards');
+const $cardContainer = document.getElementById('card-container');
+// console.log($cardContainer);
 
 //컬러리스트
 let colorArray = [
@@ -25,8 +26,6 @@ let colorArray = [
 //랜덤으로 들어갈 배열
 let color = [];
 
-
-
 //랜덤배열 만들기
 const shuffle = () => {
     for (let i = 0; colorArray.length > 0; i++) {
@@ -35,7 +34,6 @@ const shuffle = () => {
         );
     }
 }
-shuffle();
 
 //카드세팅
 const setting = (horizon, vertical) => {
@@ -44,27 +42,37 @@ const setting = (horizon, vertical) => {
         //문서객체 생성
         const $card = document.createElement("div");
         const $cardInner = document.createElement("div");
+        //카드 앞면(공통)
+        const $cardFront = document.createElement("div");
         //카드 안쪽(답)
         const $cardBack = document.createElement("div");
 
         //문서객체에 클래스 부여
         $card.classList.add("card");
         $cardInner.classList.add("card-inner");
+        $cardFront.classList.add("card-front")
         $cardBack.classList.add("card-back");
 
         //문서객체를 추가하기
-        $cards.appendChild($card);
+        $cardContainer.appendChild($card);
         $card.appendChild($cardInner);
+        $cardInner.appendChild($cardFront);
         $cardInner.appendChild($cardBack);
 
+
+        $cardFront.style.backgroundColor = "#313131"
         $cardBack.style.backgroundColor = color[i];
+        
 
         //toggle기능 부여
         $card.addEventListener("click", () => {
             $card.classList.toggle("flip");
         });
     }
+
+
 }
+shuffle();
 setting(horizon, vertical);
 
 
